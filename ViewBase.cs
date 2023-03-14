a043142217b35e0d5b12900f8f33496e12c682f5
Irzan Tri Saputra <36060059+Irzants@users.noreply.github.com>
2023-03-14 16:38:50 +0700

    Create ViewBase.cs

---
+++ ViewBase.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RichGagak.Meowtyp
+{
+    public enum UILayer
+    {
+        Common = 0,
+        Top = 1,
+        Bottom = 2,
+    }
+
+    public abstract class ViewBase : MonoBehaviour
+    {
+        protected RectTransform mRectTransform;
+        [NonSerialized] public bool isGlobal;
+        public RectTransform RectTransform => gameObject.GetComponentCached(ref mRectTransform);
+
+        internal virtual void OnOpen() { }
+        internal virtual void OnClose() { }
+        public void Close()
+        {
+            Action<ViewBase> ms = (isGlobal ? GlobalUIManager.Close : UIManager.Close);
+            ms(this);
+        }
+    }
+}
